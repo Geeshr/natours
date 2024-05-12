0a8522b41fca95904a7e03a6b6eff71c3359fc21
Geeshr <geeshni1@gmail.com>
2024-05-12 11:33:52 -0400

    test

--- app.js
+++ app.js
@@ -210,9 +210,9 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+// });
 
 app.use(globalErrorHandler);
 
