1067fc8718e7269a663aad27447c73493f263c8c
Geeshr <geeshni1@gmail.com>
2024-05-18 19:01:32 -0400

    code clean up

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -23,5 +23,4 @@ exports.webhookCheckout = (req, res, next) => {
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
-// exports.updateBooking = factory.updateOne(Booking);
 exports.deleteBooking = factory.deleteOne(Booking);
